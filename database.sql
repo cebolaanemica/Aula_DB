7c73c9a4179b03cdc37bc2b8beab08463a95c914
Robson <84165824+cebolaanemica@users.noreply.github.com>
2023-04-04 21:45:25 -0300

    aula

---
+++ database.sql
@@ -0,0 +1,14 @@
+
+
+/* create database cadastro; */
+/* use cadastro;
+create table pessoa
+(
+codPessoa int auto_increment,
+nome varchar(150),
+email varchar(100),
+datanasc date,
+primary key (codPessoa)
+);
+*/
+select * from pessoa;
