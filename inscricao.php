7bbe592bcd8959eb37775dddbffa8c35e6ce9f83
Robson <84165824+cebolaanemica@users.noreply.github.com>
2023-04-11 21:34:24 -0300

    Criando uma pagina de login
    Aula dia 11/04/2023

---
+++ inscricao.php
@@ -0,0 +1,8 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbsenha = '';
+$dbname = 'rodcad';
+$conexao = new mysqli($dbhost, $dbuser, $dbsenha, $dbname);
+
+?>
---
+++ rodcad.php
@@ -0,0 +1,11 @@
+<?php
+
+    $nick = $_POST['nick'];
+    $email = $_post
+
+
+
+
+
+
+?>
